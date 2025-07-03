f8fc57fbb26e543f3ba8d0d6772c176ec1650be1
EdgarFuentes100 <edgarfuentes139@gmail.com>
2025-07-03 13:44:50 -0700

    Se registro AppDBContext en Program.cs usando UseMySql con detección automática de versión del servidor.

--- ApiMySql/Program.cs
+++ ApiMySql/Program.cs
@@ -1,6 +1,15 @@
+using ApiWeb.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Add services to the container.
+var connection = builder.Configuration.GetConnectionString("Connection");
+
+builder.Services.AddDbContext<AppDBContext>(options =>
+    options.UseMySql(connection, ServerVersion.AutoDetect(connection)));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
