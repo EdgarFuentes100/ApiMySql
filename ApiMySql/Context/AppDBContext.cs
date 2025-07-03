4298ee895c38abf97e3156975749b1a399e01b21
EdgarFuentes100 <edgarfuentes139@gmail.com>
2025-07-03 13:44:13 -0700

    Se implemento AppDBContext con DbSet para la entidad Empleado en EF Core.

---
+++ ApiMySql/Context/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using ApiWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace ApiWeb.Context
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+
+        public DbSet<Empleado> Empleado { get; set; }
+    }
+}
