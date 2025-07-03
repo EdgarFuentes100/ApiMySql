2ea9c0e5cc25b3964267f22112d08ce6123c84c9
EdgarFuentes100 <edgarfuentes139@gmail.com>
2025-07-03 13:45:10 -0700

    Configuracion del proyecto para permitir creación y aplicación de migraciones EF Core en base de datos MySQL.

---
+++ ApiMySql/Models/Empleado.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiWeb.Models
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "El nombre debe tener al menos 3 caracteres.")]
+        public string Nombre { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "La edad debe ser un número positivo.")]
+        public int Edad { get; set; }
+
+        [Required]
+        public string Puesto { get; set; }
+
+        [Required]
+        public string Departamento { get; set; }
+    }
+}
